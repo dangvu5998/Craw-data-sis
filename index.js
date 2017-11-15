76f6fa50b832fd3fc44e07fea5c8b40fccb9acdd
trivu <dangvu5998@gmail.com>
2017-11-15 11:53:01 +0700

    get data with multi id

--- index.js
+++ index.js
@@ -8,44 +8,60 @@ var driver = new webdriver.Builder()
 function getDataById(start, end) {
   
   let index = start;
-  function getData(index) {
-    driver.findElement(By.id('MainContent_tbStudentID_I'))
-    .then(async function(el) {
-      await el.sendKeys(index);
-      await el.sendKeys(webdriver.Key.ENTER);
-      var data = {};
-      
-      await driver.wait(until.elementLocated(By.id('MainContent_gvStudents_DXDataRow0')), 1000)
-      .then(() => {
-        var field = [];
-        field[0] = 'idStudent';
-        field[1] = 'surName';
-        field[2] = 'midName';
-        field[3] = 'name';
-        field[4] = 'birth';
-        field[5] = 'class';
-        field[6] = 'eduProgram';
-        driver.findElements(By.css('#MainContent_gvStudents_DXDataRow0 td'))
-        .then(function(eles) {
-          eles.forEach((ele, i) =>{ 
-            ele.getText()
-            .then(text => data[field[i]] = text)
+  async function getData(index) {
+    var data = {};
+    return new Promise((resolve, reject) => {
+      driver.findElement(By.id('MainContent_tbStudentID_I'))
+      .then(async function(el) {
+        await el.clear();
+        await el.sendKeys(index + '\n');
+        await el.sendKeys(webdriver.Key.ENTER);
+        await driver.wait(function() {
+          return driver.wait(until.elementLocated(By.css('#MainContent_gvStudents_DXDataRow0 td')), 1000)
+          .then(function() {
+            return driver.findElement(By.css('#MainContent_gvStudents_DXDataRow0 td'))
+            .then(ele => ele.getText().then(txt => { return txt == index}))
+            .catch(err => 0)
+          })
+        }, 1000)
+        .then(() => {
+          var field = [];
+          field[0] = 'idStudent';
+          field[1] = 'surName';
+          field[2] = 'midName';
+          field[3] = 'name';
+          field[4] = 'birth';
+          field[5] = 'class';
+          field[6] = 'eduProgram';
+          driver.findElements(By.css('#MainContent_gvStudents_DXDataRow0 td'))
+          .then(function(eles) {
+            eles.forEach((ele, i) =>{ 
+              ele.getText()
+              .then(text => {if(field[i]) data[field[i]] = text;})
 
-          });
+            });
 
+          })
         })
-      })
-      .catch(err => console.log(`Not exist ${index} id`));
-      console.log(data);
+        .catch(err => reject(`Not exist id ${index}`));
+        el.clear();
+        resolve(data);
 
+      });
     })
+    
   }
   driver.get('http://sis.hust.edu.vn/ModuleSearch/GroupList.aspx')
-  .then(function() {
-
-    
-    getData(20162049);
+  .then(async function() {
+    var e = 0;
+    for(var i = start; i <= end; i++) {
+      await getData(i)
+      .then(data => console.log(data))
+      .catch(err => e++);
+    }
+    console.log(e);
   })
 }
 
-getDataById(20162900, 20163000);
+getDataById(20162900, 20162950);
+driver.quit();
