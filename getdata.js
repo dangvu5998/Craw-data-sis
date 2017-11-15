71b989e289c7ca931fdb46bd289d571e51600f2e
trivu <dangvu5998@gmail.com>
2017-11-15 23:03:37 +0700

    connect to mongodb

---
+++ getdata.js
@@ -0,0 +1,69 @@
+var webdriver = require('selenium-webdriver'),
+By = webdriver.By,
+until = webdriver.until;
+
+var driver = new webdriver.Builder()
+.forBrowser('firefox')
+.build();
+
+exports.driver = driver;
+exports.getStudentsById = function (start, end) {
+  
+  let index = start;
+  async function getData(index) {
+    var data = {};
+    return new Promise((resolve, reject) => {
+      driver.findElement(By.id('MainContent_tbStudentID_I'))
+      .then(async function(el) {
+        await el.clear();
+        await el.sendKeys(index + '\n');
+        await el.sendKeys(webdriver.Key.ENTER);
+        await driver.wait(function() {
+          return driver.wait(until.elementLocated(By.css('#MainContent_gvStudents_DXDataRow0 td')), 500)
+          .then(function() {
+            return driver.findElement(By.css('#MainContent_gvStudents_DXDataRow0 td'))
+            .then(ele => ele.getText().then(txt => { return txt == index}))
+            .catch(err => 0)
+          })
+        }, 1000)
+        .then(() => {
+          var field = [];
+          field[0] = 'idStudent';
+          field[1] = 'surName';
+          field[2] = 'midName';
+          field[3] = 'priName';
+          field[4] = 'birth';
+          field[5] = 'class';
+          field[6] = 'eduProgram';
+          driver.findElements(By.css('#MainContent_gvStudents_DXDataRow0 td'))
+          .then(function(eles) {
+            eles.forEach((ele, i) =>{ 
+              ele.getText()
+              .then(text => {if(field[i]) data[field[i]] = text;})
+
+            });
+
+          })
+        })
+        .catch(err => reject(`Not exist id ${index}`));
+        el.clear();
+        resolve(data);
+
+      });
+    })
+    
+  }
+  var students = [];
+  var invalid = [];
+  return driver.get('http://sis.hust.edu.vn/ModuleSearch/GroupList.aspx')
+  .then(async function() {
+    for(var i = start; i <= end; i++) {
+      await getData(i)
+      .then(data => students.push(data))
+      .catch(err => invalid.push(i));
+    }
+    // driver.quit();
+    return {students: students, invalid: invalid}
+  })
+}
+
--- index.js
+++ index.js
@@ -1,67 +1,31 @@
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
 
-var driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .build();
-function getDataById(start, end) {
-  
-  let index = start;
-  async function getData(index) {
-    var data = {};
-    return new Promise((resolve, reject) => {
-      driver.findElement(By.id('MainContent_tbStudentID_I'))
-      .then(async function(el) {
-        await el.clear();
-        await el.sendKeys(index + '\n');
-        await el.sendKeys(webdriver.Key.ENTER);
-        await driver.wait(function() {
-          return driver.wait(until.elementLocated(By.css('#MainContent_gvStudents_DXDataRow0 td')), 1000)
-          .then(function() {
-            return driver.findElement(By.css('#MainContent_gvStudents_DXDataRow0 td'))
-            .then(ele => ele.getText().then(txt => { return txt == index}))
-            .catch(err => 0)
-          })
-        }, 1000)
-        .then(() => {
-          var field = [];
-          field[0] = 'idStudent';
-          field[1] = 'surName';
-          field[2] = 'midName';
-          field[3] = 'name';
-          field[4] = 'birth';
-          field[5] = 'class';
-          field[6] = 'eduProgram';
-          driver.findElements(By.css('#MainContent_gvStudents_DXDataRow0 td'))
-          .then(function(eles) {
-            eles.forEach((ele, i) =>{ 
-              ele.getText()
-              .then(text => {if(field[i]) data[field[i]] = text;})
+const mongoose = require('mongoose');
+const { getStudentsById, driver } = require( './getdata');
+require('dotenv').config();
 
-            });
+mongoose.connect(process.env.MONGODB_URI);
+const Student = require('./model');
 
-          })
-        })
-        .catch(err => reject(`Not exist id ${index}`));
-        el.clear();
-        resolve(data);
 
-      });
+var startId = process.argv[2]
+var endId = process.argv[3]
+if(startId && endId) {
+  getStudentsById(startId, endId)
+  .then(function(data) {
+    driver.quit();
+    var students = data.students;
+    return students.forEach(student => {
+      var dateParts = student.birth.split('.');
+      new Student({
+        studentId: parseInt(student.idStudent),
+        priName: student.priName,
+        surName: student.surName,
+        midName: student.midName,
+        birth: new Date(dateParts[2], dateParts[1]-1, dateParts[0]),
+        class: student.class,
+        eduProgram: student.eduProgram
+      }).save(err =>{if(err) console.error(student)});
     })
-    
-  }
-  driver.get('http://sis.hust.edu.vn/ModuleSearch/GroupList.aspx')
-  .then(async function() {
-    var e = 0;
-    for(var i = start; i <= end; i++) {
-      await getData(i)
-      .then(data => console.log(data))
-      .catch(err => e++);
-    }
-    console.log(e);
   })
+  .then(() => mongoose.connection.close());
 }
-
-getDataById(20162900, 20162950);
-driver.quit();
